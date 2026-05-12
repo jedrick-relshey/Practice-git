c25680650922fc52de99f85cc3a2a6fac34ca67c
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-12 21:02:35 +0800

    change the js DOM

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ console.log(comments[5]);
 const allContent = document.querySelector("#first-heading");
 allContent.innerHTML = "This is the new content";
 
-
 const allParagraphs = document.querySelectorAll(".comment");
 allParagraphs[0].innerHTML = "This is the new content for all paragraphs";
+
+const textFirstName = document.querySelector("#textName");
+textFirstName.placeholder = "Enter your first name";
+
+const image = document.querySelector("#image-example");
+image.src = "https://www.w3schools.com/images/w3schools_green.jpg";
