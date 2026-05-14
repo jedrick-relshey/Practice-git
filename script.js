4b066d48065cd365f54b1df38683e4d470797685
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-14 19:58:19 +0800

    change the js DOM

--- script.js
+++ script.js
@@ -3,5 +3,27 @@ function sayHello() {
     title.innerHTML = "Hello, baby!";
 };
 
-const textinput = document.querySelector("#textbox");
-textinput.placeholder = "Type something here...";
+function hoverText() {
+    let hover = document.querySelector("p");
+    hover.innerHTML = "Hovering over the text!";
+};
+
+function hoverOut() {
+    let hoverout = document.querySelector("p");
+    hoverout.innerHTML = "Hover over the text!";
+};
+
+function clicktext() {
+    let hover = document.querySelector("p");
+    hover.style.color = "red";
+};
+
+function triggerFocus() {
+    const inp = document.querySelector("#textbox");
+
+    inp.style.background = "gray";
+    inp.style.color = "blue";
+};
+
+const inp = document.querySelector("#textbox");
+inp.placeholder = "Type something here...";
