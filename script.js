5fddb359387434e2114791f270159bdee3035e5d
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-14 21:38:33 +0800

    change the js DOM

--- script.js
+++ script.js
@@ -28,8 +28,29 @@ function triggerFocus() {
 const inp = document.querySelector("#textbox");
 inp.placeholder = "Type something here...";
 
-function triggerInput() {
+function triggerChange() {
     const input = document.querySelector("#box");
     const p = document.querySelector("#paragraph");
     p.innerHTML = input.value;
+};
+
+function signIn() {
+    const email = document.querySelector("#txtEmail").value;
+    const pass = document.querySelector("#txtPassword").value;
+    alert(email + "\n" + pass);
+};
+
+function submit() {
+    const submit = document.querySelector("#chkAgree").checked;
+    
+    if (submit) {
+        alert("Form submitted successfully!");
+    } else {
+        alert("You have to Click the checkbox to submit the form.");
+    };
+}; 
+
+function submitData() {
+    const sex = document.querySelector("input[name=sex]:checked").value;
+    alert(sex);
 }
