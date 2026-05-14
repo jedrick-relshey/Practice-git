e75a07dab7502c9b902988b9e10a5177c09c7719
miclatcoder2025 <miclatjedrick8@gmail.com>
2026-05-14 19:23:20 +0800

    change the js DOM

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-const body = document.querySelector('body');
-body.style.backgroundColor = "rgb(32, 32, 32)";
-body.style.margin = "0";
+function sayHello() {
+    const title = document.querySelector(".title");
+    title.innerHTML = "Hello, baby!";
+};
 
-const div = document.querySelector("div");
-div.style.backgroundColor = "black";
-div.style.paddingTop = "20px";
-div.style.paddingBottom = "15px";
-div.style.textAlign = "center";
-
-const image = document.querySelector("img");
-image.src = "/sdpt.jpg";
-image.style.width = "100px";
-image.style.textAlign = "center";
-
-const h1 = document.querySelector("h1");
-h1.innerHTML = "SDPT Academy";
-h1.style.color = "white";
-h1.style.fontFamily = "monserrat sans-serif";
-h1.style.textAlign = "center";
-h1.style.margin = "0";
-h1.style.paddingBottom = "20px";
-
-const paragraph = document.querySelector("p");
-paragraph.innerHTML = "SDPT Academy is a leading educational institution that offers a wide range of courses and programs to help students achieve their academic and career goals. With a focus on quality education and student success, SDPT Academy provides a supportive learning environment where students can thrive and reach their full potential.";
-paragraph.style.color = "white";
-paragraph.style.fontFamily = "verdana sans-serif";
-paragraph.style.textAlign = "center";
-paragraph.style.margin = "10px 20px";
+const textinput = document.querySelector("#textbox");
+textinput.placeholder = "Type something here...";
